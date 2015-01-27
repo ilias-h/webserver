61d6fddb80ed646b864ed73082bc0f7638d12393
fredoste101 <fredrik_ostdahl@hotmail.com>
2015-01-27 15:11:09 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,12 +1,52 @@
+#include<sys/types.h>
+#include<sys/fcntl.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#define SERVER_PORT 12345
+#define BUF_SIZE 4096
+#define QUEUE_SIZE 10
+
+static void fatal(char * string){
 	printf("\n%s\n",string);
 	exit(0);
+}
+
+char t[10];
+static char * parse(char * str, char c){
+		int i=0;
+
+		memset(&t,' ',sizeof(t));
+
+		while(*str != c){
+			t[i]=*str;
+			str++;
+			i++;
+		}
+	return t;
+}
+
+static int string_equal(char* str, char* str2, int size){
+	int i = 0;
+	while(i<size){
+		if(*str != *str2)
+			return 0;
+		str++;
+		str2++;
+		i++;
+	}
+	return 1;
 }
 
 int main(int argc,char *argv[]){
 
 	int s,b,l,fd,sa,bytes,on=1;
-	char buf[BUF_SIZE];
+	char buf[BUF_SIZE], * temp;
 	struct sockaddr_in channel;
 
 /* Address structure */
@@ -27,21 +67,30 @@ int main(int argc,char *argv[]){
 		fatal("Bind failed ");
 
 	l = listen(s,QUEUE_SIZE);
+
 	if(l<0)
 		fatal("Listen Failed!");
 
 /* Time to accept */
 
 	while(1){
-
-		sa = accepts(s,0,0);
+		printf("\nReady to take reuests\n");
+		sa = accept(s,0,0);
 		if(sa<0)
 			fatal("Accept failed");
 
 		read(sa,buf,BUF_SIZE);
-	
 
-		fd = open(buf,O_RDONLY);
+		printf("\n%s\n", buf);
+
+		printf("\n%s\n",parse(buf, ' '));
+
+		if(string_equal(parse(buf, ' '),"GET",3)){
+			printf("\n%s\n",parse(&buf[4],' '));
+		}
+					
+				
+		fd = open(parse(&buf[4],' '),O_RDONLY);
 		if(fd<0)
 			fatal("Open message failed!");
 
